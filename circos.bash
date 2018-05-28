9d5a8bccf48a317d005e67406416670e482b3f8e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-28 17:58:30 +0800

    Update circos.bash

--- circos.bash
+++ circos.bash
@@ -1,6 +1,5 @@
  # Karyotype generation
-more hg38.chrom.sizes |grep -P -v "_|M"|sort -k1,1 -V -s|awk -F"\t" '{print $1"\t"1"\t"$2}'| \
-perl -pe "s/^/chr\t\-\ths\t/g"|perl -pe 's/hs\tchr(.+)\t/hs$1\tchr$1\t/g'| \
-awk -F"\t" '{print $1"\t"$2"\t"$3"\t"4"\t"$5"\t"$6"\t"$7"\t"$5}' > karyotype.txt
+more hg38.chrom.sizes |grep -P -v "_|M"|sort -k1,1 -V -s|perl -pe "s/^/chr\t\-\ths\t/g"| \
+perl -pe 's/hs\tchr(.+)\t/hs$1\t$1\t1\t/g'|awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$1$4}' > karyotype.txt
 
 #
