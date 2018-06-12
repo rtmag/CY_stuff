30d0bc015d28acfe174b9f64e3a13c3afe919bad
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-12 22:31:09 +0800

    Update script_12_june.bash

--- script_12_june.bash
+++ script_12_june.bash
@@ -1,2 +1,12 @@
-perl -pe 's/chr/hs/g' deletions/data/deletions_raw/sim1_deletions.bed|perl -pe 's/\t(\w+)\t(\w+)\n/\tlabel=$2\n/g' \
-> deletions/data/deletions/sim1_deletions.bed
+perl -pe 's/chr/hs/g' simulation/groundtruth_sim1/sim1_deletions.bed|perl -pe 's/\t(\w+)\t(\w+)\n/\tlabel=$2\n/g' \
+> groundtruth/data/deletions.bed
+perl -pe 's/chr/hs/g' simulation/groundtruth_sim1/sim1_insertions.bed|perl -pe 's/\t(\w+)\t(\w+)\n/\tlabel=$2\n/g' \
+> groundtruth/data/insertions.bed
+perl -pe 's/chr/hs/g' simulation/groundtruth_sim1/sim1_inversions.bed|perl -pe 's/\t(\w+)\t(\w+)\n/\tlabel=$2\n/g' \
+> groundtruth/data/inversions.bed
+perl -pe 's/chr/hs/g' simulation/groundtruth_sim1/sim1_tandemDuplications.bed|perl -pe 's/\t(\w+)\t(\w+)\n/\tlabel=$2\n/g' \
+> groundtruth/data/tandemDuplications.bed
+perl -pe 's/chr/hs/g' simulation/groundtruth_sim1/sim1_viralinsertions.bed|perl -pe 's/\t(\w+)\t(\w+)\n/\tlabel=$2\n/g' \
+> groundtruth/data/viralinsertions.bed
+#
+
